e47d778365ddb8fddca61eb297a792b69729af3a
DwarfMaster <luc@dwarfmaster.net>
2022-12-01 18:07:53 +0100

    Compute metrics from webcomics in prolog

--- test.pl
+++ test.pl
@@ -29,18 +29,72 @@ webcomic_dom(WEBCOMIC, DOM) :-
   http_load_html(URL, DOM).
 webcomic_pages_pair(WEBCOMIC, DOM, [ TITLE, URL ]) :-
   webcomic_pages(WEBCOMIC, DOM, TITLE, URL).
-webcomic_get_pages(WEBCOMIC, PAGES) :-
-  webcomic_dom(WEBCOMIC, DOM),
+webcomic_get_pages(WEBCOMIC, DOM, PAGES) :-
   bagof(PAIR, webcomic_pages_pair(WEBCOMIC, DOM, PAIR), TMP),
   webcomic_pages_post(WEBCOMIC, TMP, PAGES).
 webcomic_chapters_pair(WEBCOMIC, DOM, [ TITLE, URL ]) :-
   webcomic_chapters(WEBCOMIC, DOM, TITLE, URL).
-webcomic_get_chapters(WEBCOMIC, CHAPTERS) :-
-  webcomic_dom(WEBCOMIC, DOM),
+webcomic_get_chapters(WEBCOMIC, DOM, CHAPTERS) :-
   bagof(PAIR, webcomic_chapters_pair(WEBCOMIC, DOM, PAIR), TMP),
   webcomic_chapters_post(WEBCOMIC, TMP, CHAPTERS).
 
+page_url([ _, URL ], URL).
+page_title([ TITLE, _ ], TITLE).
 
-sandra_pages(PAGES) :- webcomic_get_pages(sandraandwoo, PAGES).
-empowered_pages(PAGES) :- webcomic_get_pages(empowered, PAGES).
-empowered_chapters(CHAPTERS) :- webcomic_get_chapters(empowered, CHAPTERS).
+% FROM is the first page of a chapter
+chapters_metrics_find(PAGES, CHAPTERS, FROM, _, CHAP, LEFT, NEW, NEW_CHAPTERS) :-
+  append([ PAGE ], TL, PAGES),
+  append([ CHAP ], TLC, CHAPTERS),
+  page_url(PAGE, FROM), page_url(CHAP, FROM), !,
+  length(TL, NEW),
+  length(TLC, NEW_CHAPTERS),
+  chapters_metrics_compute(TL, TLC, 0, LEFT).
+% New chapter
+chapters_metrics_find(PAGES, CHAPTERS, FROM, _, CURRENT, LEFT, NEW, NEW_CHAPTERS) :-
+  append([ PAGE ], TL, PAGES),
+  append([ CHAP ], TLC, CHAPTERS),
+  page_url(PAGE, URL), page_url(CHAP, URL), !,
+  chapters_metrics_find(TL, TLC, FROM, CHAP, CURRENT, LEFT, NEW, NEW_CHAPTERS).
+% Find from page
+chapters_metrics_find(PAGES, CHAPTERS, FROM, ACTUAL, ACTUAL, LEFT, NEW, NEW_CHAPTERS) :-
+  append([ PAGE ], TL, PAGES),
+  page_url(PAGE, FROM), !,
+  length(TL, NEW),
+  length(CHAPTERS, NEW_CHAPTERS),
+  chapters_metrics_compute(TL, CHAPTERS, 0, LEFT).
+% Pass page
+chapters_metrics_find(PAGES, CHAPTERS, FROM, ACTUAL, CURRENT, LEFT, NEW, NEW_CHAPTERS) :-
+  append([ _ ], TL, PAGES), !,
+  chapters_metrics_find(TL, CHAPTERS, FROM, ACTUAL, CURRENT, LEFT, NEW, NEW_CHAPTERS).
+% Count number of pages until next chapter start
+chapters_metrics_compute(PAGES, CHAPTERS, LEFT, LEFT) :-
+  append([ PAGE ], _, PAGES),
+  append([ CHAP ], _, CHAPTERS),
+  page_url(PAGE, URL), page_url(CHAP, URL), !.
+chapters_metrics_compute(PAGES, [], N, LEFT) :-
+  length(PAGES, LEN),
+  LEFT = N + LEN.
+chapters_metrics_compute([], _, LEFT, LEFT).
+chapters_metrics_compute(PAGES, CHAPTERS, N, LEFT) :-
+  append([ _ ], TL, PAGES),
+  NN is N+1,
+  chapters_metrics_compute(TL, CHAPTERS, NN, LEFT).
+% CURRENT: current chapter
+% LEFT: number of pages left in current chapter
+% NEW: new comics in total
+% NEW_CHAPTERS: new chapters after current one
+chapters_metrics(WEBCOMIC, DOM, FROM, CURRENT, LEFT, NEW, NEW_CHAPTERS) :-
+  webcomic_get_pages(WEBCOMIC, DOM, PAGES),
+  webcomic_archive(WEBCOMIC, URL),
+  webcomic_get_chapters(WEBCOMIC, DOM, CHAPTERS),
+  chapters_metrics_find(PAGES, CHAPTERS, FROM, [ "Archive", URL ], CURRENT, LEFT, NEW, NEW_CHAPTERS).
+
+empowered_pages(PAGES) :-
+  file_load_html("empowered.html", DOM),
+  webcomic_get_pages(empowered, DOM, PAGES).
+empowered_chapters(CHAPTERS) :-
+  file_load_html("empowered.html", DOM),
+  webcomic_get_chapters(empowered, DOM, CHAPTERS).
+empowered_metrics(CHAP, L, N, NC) :-
+  file_load_html("empowered.html", DOM),
+  chapters_metrics(empowered, DOM, "https://www.empoweredcomic.com/comic/volume-2-page-80", CHAP, L, N, NC).
--- webcomics.pl
+++ webcomics.pl
@@ -9,29 +9,29 @@
 :- discontiguous webcomic_chapters_post/3.
 
 % Sandra And Woo
-webcomic_archive(sandraandwoo, 'http://www.sandraandwoo.com/archive/').
+webcomic_archive(sandraandwoo, "http://www.sandraandwoo.com/archive/").
 webcomic_pages(sandraandwoo, DOM, TITLE, URL) :-
   xpath(DOM, //td(@class='archive-title')/a, A),
-  xpath(A, /a(@href), URL),
-  xpath(A, /a(text), TITLE).
+  xpath(A, /a(@href), URL_ATOM), atom_string(URL_ATOM, URL),
+  xpath(A, /a(text), TITLE_ATOM), atom_string(TITLE_ATOM, TITLE).
 webcomic_pages_post(sandraandwoo, IN, OUT) :-
   reverse(IN, OUT).
 webcomic_chapters_post(sandraandwoo, IN, IN).
 
 % Empowered
 empowered_url(REL, URL) :-
-  string_concat('https://www.empoweredcomic.com/', REL, URL).
-webcomic_archive(empowered, 'https://www.empoweredcomic.com/comic/archive').
+  string_concat("https://www.empoweredcomic.com/", REL, URL).
+webcomic_archive(empowered, "https://www.empoweredcomic.com/comic/archive").
 webcomic_pages(empowered, DOM, TITLE, URL) :-
   xpath(DOM, //select(@name='comic')/option, OPT),
   xpath(OPT, /self(@value), RELATIVE),
   string_length(RELATIVE, REL_LEN),
   REL_LEN > 0,
   empowered_url(RELATIVE, URL),
-  xpath(OPT, /self(content), TITLE).
+  xpath(OPT, /self(content), [TITLE_ATOM]), atom_string(TITLE_ATOM, TITLE).
 webcomic_pages_post(empowered, IN, IN).
 webcomic_chapters(empowered, DOM, TITLE, URL) :-
   xpath(DOM, //div(@class='cc-storyline-contain')/div/div(@class='cc-storyline-header')/a, A),
-  xpath(A, /self(@href), URL),
-  xpath(A, /self(content), TITLE).
+  xpath(A, /self(@href), URL_ATOM), atom_string(URL_ATOM, URL),
+  xpath(A, /self(content), [TITLE_ATOM]), atom_string(TITLE_ATOM, TITLE).
 webcomic_chapters_post(empowered, IN, IN).

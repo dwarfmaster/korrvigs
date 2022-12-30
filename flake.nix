3b96d96c03bdbfabc4f63281d6b68ba0f42304c2
DwarfMaster <luc@dwarfmaster.net>
2022-12-30 22:17:19 +0100

    Plug norg treesitter into swi-prolog

--- flake.nix
+++ flake.nix
@@ -5,15 +5,31 @@
     self,
     nixpkgs,
   }: let
+
     pkgs = import nixpkgs {system = "x86_64-linux";};
+
     gems = pkgs.bundlerEnv {
       name = "asciidoc-knowledge-database";
       gemdir = ./extractor;
     };
+
+    norg-source = pkgs.fetchFromGitHub {
+      owner = "nvim-neorg";
+      repo = "tree-sitter-norg";
+      rev = "600874e0825a62c3822ff04e5d0efbac8d7e380c";
+      sha256 = "1l200zd7xfhifaxq3i810j2bxpvm0yfgf6x2i2d83l34z32q3p4x";
+    };
+
     shell = pkgs.mkShell {
-      packages = [gems.wrappedRuby gems pkgs.swiProlog pkgs.python3 pkgs.tree-sitter];
+      packages = [
+        gems.wrappedRuby
+        gems
+        pkgs.swiProlog
+        pkgs.python3
+      ];
       PYTHONPATH = "${pkgs.swiProlog}/lib/swipl/lib";
-      PKG_CONFIG_PATH = "${pkgs.swiProlog}/share/pkgconfig";
+      PKG_CONFIG_PATH = "${pkgs.swiProlog}/share/pkgconfig:${pkgs.tree-sitter}/lib/pkgconfig";
+      NORG_PARSER_SOURCE = "${norg-source}/src";
     };
     update = pkgs.mkShell {
       packages = [pkgs.bundix pkgs.ruby];
--- tree-sitter/norg-parser.pl
+++ tree-sitter/norg-parser.pl
@@ -1,5 +1,30 @@
 
 :- module('norg-parser',
-         [ get_hello/1
+         [ parse_norg/2
+         , norg_link/2
          ]).
 :- use_foreign_library('norg_parser.so').
+:- use_module(library(xpath)).
+
+parse_norg(stream(STREAM), DOM) :-
+  read_string(STREAM, "", "", _, STR),
+  parse_norg_impl(STR, DOM).
+parse_norg(file(PATH), DOM) :-
+  open(PATH, read, STREAM, []),
+  parse_norg(stream(STREAM), DOM).
+parse_norg(STR, DOM) :- parse_norg_impl(STR, DOM).
+
+% Some query examples
+norg_link(DOM, [TYPE, TARGET, NAME]) :-
+  norg_link_kind(DOM, LINK),
+  xpath(LINK, //link_location/'*'(@field="type"), element(TYPE, _, _)),
+  xpath(LINK, //link_location/'*'(@field="text",text), TARGET),
+  ( xpath(LINK, //link_description/'*'(@field="text",text), NAME) -> true ; NAME = TARGET ).
+
+norg_link_kind(DOM, LINK) :-
+  xpath(DOM, //link(content), LINK).
+norg_link_kind(DOM, LINK) :-
+  xpath(DOM, //anchor_definition(content), LINK).
+
+
+

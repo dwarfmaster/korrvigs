665dbf151972bd9d8e6e3cf5bfd4052cab4e6e99
DwarfMaster <luc@dwarfmaster.net>
2023-01-01 21:29:13 +0100

    Add action to select actions

--- engine/modules/actions.pl
+++ engine/modules/actions.pl
@@ -1,10 +1,8 @@
 
 :- module(actions, []).
 :- multifile register/3.
-
-%! register(+PRIORITY, +DESC, +CODE)
-%  Each success is an action with a priority, a description and a code
-%  to execute. It should fail if the action is not meant to be applied.
+:- use_module(korrvigs(fzf)).
+:- use_module(korrvigs(ctx)).
 
 %! list(+ACTIONS)
 %  Get a list of actions ordered by score. An action is a name and
@@ -13,10 +11,24 @@ find_action([ PRIORITY, DESC, CODE ]) :-
   register(PRIORITY, DESC, CODE).
 tail(LIST, TL) :- append([_], TL, LIST).
 list(ACTIONS) :-
-  bagof(ACT, find_action(ACT), ACTS),
+  bagof(ACT, find_action(ACT), ACTS), !,
   sort(1, @>=, ACTS, SORTED),
   maplist(tail, SORTED, ACTIONS).
+list([]).
 
 %! run_action(-ACTION)
 %  Execute the code of an action.
 run([_,CODE]) :- call(CODE).
+
+%! select
+%  Use fzf to select an action to run.
+select :-
+  list(ACTS),
+  maplist(reverse, ACTS, OPTS),
+  fzf:select(OPTS, CODE),
+  call(CODE).
+
+%! register(+PRIORITY, +DESC, +CODE)
+%  Each success is an action with a priority, a description and a code
+%  to execute. It should fail if the action is not meant to be applied.
+register(0, "Select an action to run", actions:select) :- ctx:get(desktop, true).

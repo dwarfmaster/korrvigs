bcabe098d6724726ab02d22aa41459a9e9b65308
DwarfMaster <luc@dwarfmaster.net>
2023-01-12 19:48:25 +0100

    Split engine main into config and logic

---
+++ engine/config.pl
@@ -0,0 +1,18 @@
+
+user:file_search_path(korrvigs, "/home/luc/repos/korrvigs/engine/modules").
+user:file_search_path(foreign, "/home/luc/repos/korrvigs/tree-sitter").
+user:file_search_path(library, "/home/luc/repos/korrvigs/tree-sitter").
+user:file_search_path(foreign, "/home/luc/repos/korrvigs/engine/posix").
+user:file_search_path(library, "/home/luc/repos/korrvigs/engine/posix").
+
+%! data_dir(+DIR) is det
+%  data_dir indicates the directory in which the wiki files must be looked up
+data_dir("/home/luc/downloads/wiki").
+%! piper(+PATH) is det
+%  Path to the piper script
+piper("/home/luc/repos/korrvigs/piper/piper.out").
+%! spawn_terminal(-CMD)
+%  run a terminal executing command
+spawn_terminal(CMD) :-
+  absolute_file_name(path(bash), BASH),
+  process_create(path(st), [ "-e", BASH, "-c", CMD ], [ detached(true) ]).
--- engine/main.pl
+++ engine/main.pl
@@ -1,14 +1,6 @@
 
 
-%  __  __           _       _           
-% |  \/  | ___   __| |_   _| | ___  ___ 
-% | |\/| |/ _ \ / _` | | | | |/ _ \/ __|
-% | |  | | (_) | (_| | |_| | |  __/\__ \
-% |_|  |_|\___/ \__,_|\__,_|_|\___||___/
-%                                       
-user:file_search_path(korrvigs, "/home/luc/repos/korrvigs/engine/modules").
-user:file_search_path(library, "/home/luc/repos/korrvigs/tree-sitter").
-user:file_search_path(library, "/home/luc/repos/korrvigs/engine/posix").
+% Assumes config has been loaded first
 
 %! find_module(+PATH)
 %  find a module
@@ -24,28 +16,6 @@ find_module(PATH) :-
 %  Load all modules
 load_modules() :- forall(find_module(PATH), use_module(PATH)).
 
-
-%   ____             __ _       
-%  / ___|___  _ __  / _(_) __ _ 
-% | |   / _ \| '_ \| |_| |/ _` |
-% | |__| (_) | | | |  _| | (_| |
-%  \____\___/|_| |_|_| |_|\__, |
-%                         |___/ 
-
-%! data_dir(+DIR) is det
-%  data_dir indicates the directory in which the wiki files must be looked up
-data_dir("/home/luc/downloads/wiki").
-%! piper(+PATH) is det
-%  Path to the piper script
-piper("/home/luc/repos/korrvigs/piper/piper.out").
-%! spawn_terminal(-CMD)
-%  run a terminal executing command
-spawn_terminal(CMD) :-
-  absolute_file_name(path(bash), BASH),
-  process_create(path(st), [ "-e", BASH, "-c", CMD ], [ detached(true) ]).
-
-
-
 %  ____       _               
 % / ___|  ___| |_ _   _ _ __  
 % \___ \ / _ \ __| | | | '_ \ 
@@ -56,3 +26,4 @@ spawn_terminal(CMD) :-
 :- load_modules().
 :- xdg:runtime(RT), set_prolog_flag(tmp_dir, RT).
 :- wiki:reload_all().
+:- server:run().

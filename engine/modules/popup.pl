fedcfbe29fa421653e61e2a40b31eed7e3b667da
DwarfMaster <luc@dwarfmaster.net>
2023-01-20 20:30:40 +0100

    Fix error in popup

--- engine/modules/popup.pl
+++ engine/modules/popup.pl
@@ -12,7 +12,7 @@ server_is_ready(_).
 
 %! with(+CMD, +ARGS, +PRED, -OUT)
 %  Open a popup running cmd and args, and run PRED/3 on the input and output
-%  stream of the running process, unifying OUT with the second argument of
+%  stream of the running process, unifying OUT with the last argument of
 %  PRED.
 with(CMD, ARGS, PRED, RES) :-
   piper(PIPER), current_prolog_flag(pid, CPID),
@@ -20,18 +20,22 @@ with(CMD, ARGS, PRED, RES) :-
   unix_domain_socket(SOCK),
   setup_call_cleanup(
     ( temp_sock(SPATH) ),
-    ( % Wait for server to be ready
-      on_signal(usr1, _, server_is_ready),
-      process_create(path(st), NARGS, [ process(PID) ]),
-      pause(),
-      on_signal(usr1, _, default),
+    (setup_call_cleanup(
+      ( on_signal(usr1, _, server_is_ready) ),
+      (setup_call_cleanup(
+        ( process_create(path(st), NARGS, [ process(PID), stderr(null) ]) ),
+        ( % Wait for server to be ready
+          pause(),
+          on_signal(usr1, _, default),
 
-      % Connect to server
-      tcp_connect(SOCK, SPATH),
-      tcp_open_socket(SOCK, SPAIR),
-      stream_pair(SPAIR, OUT, IN),
-      !, call(PRED, IN, OUT, RES) ),
-    ( on_signal(usr1, _, default),
-      process_wait(PID, _), 
-      close(SPAIR), 
-      delete_file(SPATH) )).
+          % Connect to server
+          setup_call_cleanup(
+            ( tcp_connect(SOCK, SPATH),
+              tcp_open_socket(SOCK, SPAIR),
+              stream_pair(SPAIR, OUT, IN) ),
+            ( !, call(PRED, IN, OUT, RES) ),
+            ( close(SPAIR) ))),
+        ( process_wait(PID, RET) ))),
+      ( on_signal(usr1, _, default) ))),
+    ( delete_file(SPATH) )),
+  RET = exit(0).

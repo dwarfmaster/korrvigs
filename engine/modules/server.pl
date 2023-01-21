97444599439a0f63742a9db2b77030e8a0d9de0f
DwarfMaster <luc@dwarfmaster.net>
2023-01-21 18:01:53 +0100

    Add plugin system for server

--- engine/modules/server.pl
+++ engine/modules/server.pl
@@ -8,10 +8,11 @@
 :- use_module(korrvigs(ctx)).
 
 ctx_from_request(REQ) :-
-  member(search(SEARCH), REQ),
+  member(search(SEARCH), REQ), !,
   forall(
     member(CTX = VAL, SEARCH),
     ctx:set(CTX, VAL)).
+ctx_from_request(_).
 
 actions_handler(REQ) :-
   ctx_from_request(REQ),
@@ -25,6 +26,22 @@ actions_handler(REQ) :-
 actions_handler(_) :-
   format("Content-Type: text/json~n~n"),
   format("[]~n").
+
+registered_handler(PRED, REQ) :-
+  ctx_from_request(REQ),
+  format("Content-Type: test/plain~n~n"),
+  ( catch(
+      call(PRED),
+      EXCEPT,
+      ( format("~w~n", EXCEPT), fail ))
+    -> format("Success~n")
+    ;  format("Failure~n") ).
+
+register(NAME, PRED) :-
+  concat("actions/", NAME, PATH),
+  http_handler(root(PATH), registered_handler(PRED), [ methods([get]), spawn() ]).
+
+:- register("select", actions:select).
 
 run :-
   http_handler(root(actions), actions_handler, [ methods([get]), spawn() ]),

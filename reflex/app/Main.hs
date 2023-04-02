e01b8decdb6d86a150f80a595ba7f06b30b98a53
DwarfMaster <luc@dwarfmaster.net>
2023-04-02 13:02:21 +0200

    Add file watching abilities to reflex

--- reflex/app/Main.hs
+++ reflex/app/Main.hs
@@ -5,18 +5,27 @@
 
 module Main where
 
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
 import Reflex
 import Reflex.Host.Headless
+import qualified System.FSNotify as FS
+import System.FilePath.Posix (takeBaseName)
+
+setupWatch :: MonadHeadlessApp t m => m (Event t FS.Event)
+setupWatch = do
+  (fsEvent, fsTrigger) <- newTriggerEvent
+  _ <- liftIO . forkIO . FS.withManager $ \mgr -> do
+    _ <- FS.watchTree mgr "test" (const True) fsTrigger
+    forever $ threadDelay 1000000
+  return fsEvent
 
 setupApp :: MonadHeadlessApp t m => m (Event t ())
 setupApp = do
-  (inputEvent, inputTrigger) <- newTriggerEvent
-  _ <- liftIO . forkIO . forever $ getLine >>= inputTrigger
-  performEvent_ $ liftIO . putStrLn <$> inputEvent
-  return $ () <$ ffilter (\str -> str == "quit") inputEvent
+  watch <- setupWatch
+  performEvent_ $ liftIO . putStrLn . show <$> watch
+  return $ () <$ ffilter (\ev -> takeBaseName (FS.eventPath ev) == "quit") watch
 
 main :: IO ()
 main = runHeadlessApp setupApp

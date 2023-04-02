8d8dc5ebc1c4c40eb2a13b34ff51fb166c324cb0
DwarfMaster <luc@dwarfmaster.net>
2023-04-02 10:45:18 +0200

    Setup minimal running headless reflex app

--- reflex/app/Main.hs
+++ reflex/app/Main.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Main where
 
+import Control.Concurrent (forkIO)
+import Control.Monad (forever)
+import Control.Monad.IO.Class (liftIO)
+import Reflex
+import Reflex.Host.Headless
+
+setupApp :: MonadHeadlessApp t m => m (Event t ())
+setupApp = do
+  (inputEvent, inputTrigger) <- newTriggerEvent
+  _ <- liftIO . forkIO . forever $ getLine >>= inputTrigger
+  performEvent_ $ liftIO . putStrLn <$> inputEvent
+  return $ () <$ ffilter (\str -> str == "quit") inputEvent
+
 main :: IO ()
-main = putStrLn "Hello, Nix!"
+main = runHeadlessApp setupApp

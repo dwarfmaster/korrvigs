7ef13315fba6ab05395bc1ba149a78ff8d3fd01b
DwarfMaster <luc@dwarfmaster.net>
2023-04-02 13:00:35 +0200

    Merge the two flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,19 @@
 {
   description = "Intelligent knowledge database system";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    devenv.url = "github:cachix/devenv";
+  };
+
   outputs = {
     self,
     nixpkgs,
-  }: let
+    devenv,
+  } @ inputs: let
+    system = "x86_64-linux";
 
-    pkgs = import nixpkgs {system = "x86_64-linux";};
+    pkgs = nixpkgs.legacyPackages.${system};
 
     norg-source = pkgs.fetchFromGitHub {
       owner = "nvim-neorg";
@@ -14,40 +21,70 @@
       rev = "600874e0825a62c3822ff04e5d0efbac8d7e380c";
       sha256 = "1l200zd7xfhifaxq3i810j2bxpvm0yfgf6x2i2d83l34z32q3p4x";
     };
-    norg-parser = pkgs.callPackage ./tree-sitter { tree-sitter-norg-source = norg-source; };
+    norg-parser = pkgs.callPackage ./tree-sitter {tree-sitter-norg-source = norg-source;};
 
-    shell = pkgs.mkShell {
-      packages = [
-        pkgs.swiProlog
-        pkgs.tree-sitter
+    shell = devenv.lib.mkShell {
+      inherit inputs pkgs;
+      modules = [
+        {
+          languages.nix.enable = true;
+          languages.haskell.enable = true;
+          languages.haskell.package =
+            pkgs.haskellPackages.ghcWithPackages
+            (hpkgs: [
+              hpkgs.reflex
+              hpkgs.fsnotify
+            ]);
+
+          pre-commit.hooks = {
+            alejandra.enable = true;
+            deadnix.enable = true;
+            cabal-fmt.enable = true;
+            cabal2nix.enable = true;
+            ormolu.enable = true;
+          };
+          pre-commit.settings = {
+            alejandra.exclude = ["reflex/default.nix"];
+          };
+
+          packages = [
+            pkgs.swiProlog
+            pkgs.tree-sitter
+          ];
+          env = {
+            PYTHONPATH = "${pkgs.swiProlog}/lib/swipl/lib";
+            PKG_CONFIG_PATH = "${pkgs.swiProlog}/share/pkgconfig:${pkgs.tree-sitter}/lib/pkgconfig";
+            NORG_PARSER_SOURCE = "${norg-source}/src";
+          };
+        }
       ];
-      PYTHONPATH = "${pkgs.swiProlog}/lib/swipl/lib";
-      PKG_CONFIG_PATH = "${pkgs.swiProlog}/share/pkgconfig:${pkgs.tree-sitter}/lib/pkgconfig";
-      NORG_PARSER_SOURCE = "${norg-source}/src";
     };
 
     piper = pkgs.callPackage ./piper {};
     posix = pkgs.callPackage ./posix {};
     engine = pkgs.callPackage ./engine {};
   in {
-    devShells."x86_64-linux" = {
+    devShells.${system} = {
       default = shell;
     };
 
-    packages."x86_64-linux" = {
+    packages.${system} = {
       default = engine;
       korrvigs-norg-parser = norg-parser;
       korrvigs-posix = posix;
       korrvigs = engine;
+      backend = pkgs.haskellPackages.callPackage ./reflex {};
       inherit piper;
     };
 
-    overlays.default = final: prev: {
-      inherit (self.packages."x86_64-linux")
+    overlays.default = _: _: {
+      inherit
+        (self.packages.${system})
         korrvigs
         korrvigs-posix
         korrvigs-norg-parser
-        piper;
+        piper
+        ;
     };
 
     hmModules = {
--- reflex/default.nix
+++ reflex/default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, lib, reflex }:
+{ mkDerivation, base, filepath, fsnotify, lib, reflex }:
 mkDerivation {
   pname = "backend";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base reflex ];
+  executableHaskellDepends = [ base filepath fsnotify reflex ];
   description = "Knowledge management system";
   license = lib.licenses.mit;
   mainProgram = "backend";

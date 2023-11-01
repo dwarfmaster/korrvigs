4206293e23f2fbe75d00ff00203fd9e472f9cd75
DwarfMaster <luc@dwarfmaster.net>
2023-11-01 23:13:09 +0100

    Add default css

--- haskell/app/Korrvigs/Web/Backend.hs
+++ haskell/app/Korrvigs/Web/Backend.hs
@@ -3,6 +3,7 @@ module Korrvigs.Web.Backend where
 import Data.Text (Text)
 import Database.PostgreSQL.Simple (Connection)
 import Korrvigs.Web.Routes
+import Text.Cassius (cassiusFile)
 import qualified Web.ClientSession as CS
 import Yesod
 
@@ -19,8 +20,35 @@ jquery =
     "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
     [("integrity", "sha384-1H217gwSVyLSIfaLxHbE7dRb3v4mYCKbpQvzx0cegeju1MVsGrX5xXxAvs/HgeFs"), ("crossorigin", "anonymous")]
 
+getBase :: Handler (Int -> Text)
+getBase = pure base
+  where
+    base :: Int -> Text
+    base 0 = "#231e18"
+    base 1 = "#302b25"
+    base 2 = "#48413a"
+    base 3 = "#9d8b70"
+    base 4 = "#b4a490"
+    base 5 = "#cabcb1"
+    base 6 = "#d7c8bc"
+    base 7 = "#e4d4c8"
+    base 8 = "#d35c5c"
+    base 9 = "#ca7f32"
+    base 10 = "#e0ac16"
+    base 11 = "#b7ba53"
+    base 12 = "#6eb958"
+    base 13 = "#88a4d3"
+    base 14 = "#bb90e2"
+    base 15 = "b49368"
+    base _ = error "Unsupported base16 color"
+
+css :: (Int -> Text) -> Widget
+css base = toWidget $(cassiusFile "app/Korrvigs/Web/Ressources/css/default.cassius")
+
 instance Yesod Korrvigs where
   defaultLayout w = do
+    base <- getBase
+    let widget = sequence_ [css base, jquery, w]
     p <- widgetToPageContent widget
     msgs <- getMessages
     withUrlRenderer
@@ -34,12 +62,11 @@ instance Yesod Korrvigs where
                   <meta name="description" content="#{description}">
                 ^{pageHead p}
             <body>
-                $forall (status, msg) <- msgs
-                    <p class="message #{status}">#{msg}
-                ^{pageBody p}
+                <div #central>
+                    $forall (status, msg) <- msgs
+                        <p class="message #{status}">#{msg}
+                    ^{pageBody p}
         |]
-    where
-      widget = w >> jquery
   jsLoader _ = BottomOfBody
   makeSessionBackend _ = strictSameSiteSessions def
     where

460395a491350b4fa16ccc1a4bb9722e46fe9214
DwarfMaster <luc@dwarfmaster.net>
2023-11-26 16:33:12 +0100

    Regenerate classes and relations

--- haskell/app/Korrvigs/Classes/Generated.hs
+++ haskell/app/Korrvigs/Classes/Generated.hs
@@ -4,7 +4,7 @@ module Korrvigs.Classes.Generated where
 import Data.Ix (Ix)
 import Data.Text (Text)
 
-data Class = Entity | Continuant | ContinuousTimeRegion | DataFormatSpecification | DataItem | DirectiveInformationEntity | DiscontinuousTimeRegion | File | GenericallyDependentContinuant | Identifier | IndependentContinuant | InformationContentEntity | Namespace | NarrativeObject | Occurrent | OntologyClass | OntologyRelation | OntologySpecificationItem | Script | SpecificallyDependentContinuant | TemporalRegion | TextFile deriving (Show, Eq, Enum, Bounded, Ord, Ix)
+data Class = Entity | Continuant | ContinuousTimeRegion | DataFormatSpecification | DataItem | DirectiveInformationEntity | DiscontinuousTimeRegion | File | GenericallyDependentContinuant | Identifier | ImmaterialEntity | IndependentContinuant | InformationContentEntity | Namespace | NarrativeObject | Occurrent | OntologyClass | OntologyRelation | OntologySpecificationItem | Process | Script | SpatialRegion | SpatiotemporalRegion | SpecificallyDependentContinuant | TemporalRegion | TextFile deriving (Show, Eq, Enum, Bounded, Ord, Ix)
 
 name :: Class -> Text
 name Entity = "Entity"
@@ -17,6 +17,7 @@ name DiscontinuousTimeRegion = "Discontinuous time region"
 name File = "File"
 name GenericallyDependentContinuant = "Generically dependent continuant"
 name Identifier = "Identifier"
+name ImmaterialEntity = "Immaterial entity"
 name IndependentContinuant = "Independent continuant"
 name InformationContentEntity = "Information content entity"
 name Namespace = "Namespace"
@@ -25,7 +26,10 @@ name Occurrent = "Occurrent"
 name OntologyClass = "Ontology class"
 name OntologyRelation = "Ontology relation"
 name OntologySpecificationItem = "Ontology specification item"
+name Process = "Process"
 name Script = "Script"
+name SpatialRegion = "Spatial region"
+name SpatiotemporalRegion = "Spatiotemporal region"
 name SpecificallyDependentContinuant = "Specifically dependent continuant"
 name TemporalRegion = "Temporal region"
 name TextFile = "Text file"
@@ -41,6 +45,7 @@ parse "Discontinuous time region" = Just DiscontinuousTimeRegion
 parse "File" = Just File
 parse "Generically dependent continuant" = Just GenericallyDependentContinuant
 parse "Identifier" = Just Identifier
+parse "Immaterial entity" = Just ImmaterialEntity
 parse "Independent continuant" = Just IndependentContinuant
 parse "Information content entity" = Just InformationContentEntity
 parse "Namespace" = Just Namespace
@@ -49,7 +54,10 @@ parse "Occurrent" = Just Occurrent
 parse "Ontology class" = Just OntologyClass
 parse "Ontology relation" = Just OntologyRelation
 parse "Ontology specification item" = Just OntologySpecificationItem
+parse "Process" = Just Process
 parse "Script" = Just Script
+parse "Spatial region" = Just SpatialRegion
+parse "Spatiotemporal region" = Just SpatiotemporalRegion
 parse "Specifically dependent continuant" = Just SpecificallyDependentContinuant
 parse "Temporal region" = Just TemporalRegion
 parse "Text file" = Just TextFile
@@ -66,6 +74,7 @@ isA DiscontinuousTimeRegion = TemporalRegion
 isA File = SpecificallyDependentContinuant
 isA GenericallyDependentContinuant = Continuant
 isA Identifier = InformationContentEntity
+isA ImmaterialEntity = IndependentContinuant
 isA IndependentContinuant = Continuant
 isA InformationContentEntity = GenericallyDependentContinuant
 isA Namespace = NarrativeObject
@@ -74,7 +83,10 @@ isA Occurrent = Entity
 isA OntologyClass = OntologySpecificationItem
 isA OntologyRelation = OntologySpecificationItem
 isA OntologySpecificationItem = DataItem
+isA Process = Occurrent
 isA Script = TextFile
+isA SpatialRegion = ImmaterialEntity
+isA SpatiotemporalRegion = Occurrent
 isA SpecificallyDependentContinuant = Continuant
 isA TemporalRegion = Occurrent
 isA TextFile = File
--- haskell/app/Korrvigs/Relations/Generated.hs
+++ haskell/app/Korrvigs/Relations/Generated.hs
@@ -43,3 +43,82 @@ identifierRel =
           tableField "val"
         )
     )
+
+occupiesSpatialRegionAtRel ::
+  Table
+    (Field SqlInt8, Field SqlInt8, Field SqlInt8)
+    (Field SqlInt8, Field SqlInt8, Field SqlInt8)
+occupiesSpatialRegionAtRel =
+  table
+    "occupies_spatial_region_at"
+    ( p3
+        ( tableField "continuant",
+          tableField "region",
+          tableField "time"
+        )
+    )
+
+occupiesSpatiotemporalRegionRel ::
+  Table
+    (Field SqlInt8, Field SqlInt8)
+    (Field SqlInt8, Field SqlInt8)
+occupiesSpatiotemporalRegionRel =
+  table
+    "occupies_spatiotemporal_region"
+    ( p2
+        ( tableField "occurrent",
+          tableField "region"
+        )
+    )
+
+occupiesTemporalRegionRel ::
+  Table
+    (Field SqlInt8, Field SqlInt8)
+    (Field SqlInt8, Field SqlInt8)
+occupiesTemporalRegionRel =
+  table
+    "occupies_temporal_region"
+    ( p2
+        ( tableField "continuant",
+          tableField "region"
+        )
+    )
+
+occurrentPartOfRel ::
+  Table
+    (Field SqlInt8, Field SqlInt8)
+    (Field SqlInt8, Field SqlInt8)
+occurrentPartOfRel =
+  table
+    "occurrent_part_of"
+    ( p2
+        ( tableField "part",
+          tableField "whole"
+        )
+    )
+
+spatiallyProjectsIntoRel ::
+  Table
+    (Field SqlInt8, Field SqlInt8)
+    (Field SqlInt8, Field SqlInt8)
+spatiallyProjectsIntoRel =
+  table
+    "spatially_projects_into"
+    ( p2
+        ( tableField "region",
+          tableField "space"
+        )
+    )
+
+temporallyProjectsIntoRel ::
+  Table
+    (Field SqlInt8, Field SqlInt8)
+    (Field SqlInt8, Field SqlInt8)
+temporallyProjectsIntoRel =
+  table
+    "temporally_projects_into"
+    ( p2
+        ( tableField "region",
+          tableField "time"
+        )
+    )

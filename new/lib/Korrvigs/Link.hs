938a9c82bf1bd7a638451894fb59c836363623be
DwarfMaster <luc@dwarfmaster.net>
2024-04-01 16:14:36 +0200

    Implement link loading

--- new/lib/Korrvigs/Link.hs
+++ new/lib/Korrvigs/Link.hs
@@ -7,15 +7,18 @@ import qualified Data.Set as S
 import Korrvigs.Entry
 import Korrvigs.Kind
 import Korrvigs.KindData
+import Korrvigs.Link.SQL
 import Korrvigs.Link.Sync
 import Prelude hiding (readFile)
 
 instance IsKD Link where
   data KDMaker Link = LinkMaker
+    deriving (Ord, Eq)
   data KDUpdater Link = LinkUpdater
+    deriving (Ord, Eq)
   data KDIdentifier Link = LinkIdentifier FilePath
     deriving (Ord, Eq)
-  dLoad = undefined
+  dLoad = dLoadImpl
   dMake = undefined
   dUpdate = undefined
   dList = S.map LinkIdentifier <$> dListImpl
--- new/lib/Korrvigs/Link/SQL.hs
+++ new/lib/Korrvigs/Link/SQL.hs
@@ -7,8 +7,9 @@ import Control.Lens
 import Data.Profunctor.Product.Default
 import Data.Profunctor.Product.TH (makeAdaptorAndInstanceInferrable)
 import Data.Text (Text)
-import Korrvigs.Entry (nameKindField)
+import Korrvigs.Entry
 import Korrvigs.Kind
+import Korrvigs.Monad
 import Opaleye
 
 data LinkRowImpl a b c d = LinkRow
@@ -40,3 +41,21 @@ linksTable =
         (tableField "protocol")
         (tableField "ref")
         (tableField "file")
+
+dLoadImpl :: MonadKorrvigs m => Id -> ((Entry -> Link) -> Entry) -> m (Maybe Entry)
+dLoadImpl i cstr = do
+  sel <- rSelectOne $ do
+    lrow <- selectTable linksTable
+    where_ $ lrow ^. sqlLinkName .== sqlStrictText (unId i)
+    pure lrow
+  case (sel :: Maybe LinkRow) of
+    Nothing -> pure Nothing
+    Just lrow -> do
+      let lnk entry =
+            MkLink
+              { _linkEntry = entry,
+                _linkProtocol = lrow ^. sqlLinkProtocol,
+                _linkRef = lrow ^. sqlLinkRef,
+                _linkPath = lrow ^. sqlLinkFile
+              }
+      pure $ Just $ cstr lnk

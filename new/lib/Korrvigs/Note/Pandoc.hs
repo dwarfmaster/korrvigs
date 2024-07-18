0d9ee98ede043f800f66bf71b397cd47ea242ae5
DwarfMaster <luc@dwarfmaster.net>
2024-07-18 15:02:33 +0200

    Register outgoing references when parsing pandoc

--- new/lib/Korrvigs/Note/Pandoc.hs
+++ new/lib/Korrvigs/Note/Pandoc.hs
@@ -2,6 +2,7 @@ module Korrvigs.Note.Pandoc (parsePandoc) where
 
 import Control.Arrow ((&&&))
 import Control.Lens hiding ((<|))
+import Control.Monad.Extra (concatMapM)
 import Control.Monad.Loops (whileJust_, whileM_)
 import Control.Monad.State.Lazy
 import Data.Aeson hiding ((.=))
@@ -49,8 +50,8 @@ getBlock = blks %%= (listToMaybe &&& tail)
 pushBlock :: WithParent A.Block -> ParseM ()
 pushBlock blk = stack . bszLeft %= (blk :)
 
-pushHeader :: Int -> A.Attr -> Text -> ParseM ()
-pushHeader lvl attr title = stack %= BSZ lvl attr title S.empty [] . Just
+pushHeader :: Int -> A.Attr -> ParseM ()
+pushHeader lvl attr = stack %= BSZ lvl attr "" S.empty [] . Just
 
 access :: Getting a ParseState a -> ParseM a
 access l = get <&> (^. l)
@@ -72,25 +73,29 @@ bszToHeader (BSZ lvl attr title ref bks _) doc parent =
           }
    in hd
 
+refTo :: Id -> ParseM ()
+refTo i = stack . bszRefTo %= S.insert i
+
 popHeader :: ParseM ()
 popHeader = do
   bsz <- access stack
   case bsz ^. bszParent of
     Just parent -> do
       stack .= parent
+      stack . bszRefTo %= S.union (bsz ^. bszRefTo)
       pushBlock $ \doc hd -> A.Sub (bszToHeader bsz doc hd)
     Nothing -> pure ()
 
-startHeader :: Int -> A.Attr -> Text -> ParseM ()
-startHeader lvl attr title = do
+startHeader :: Int -> A.Attr -> ParseM ()
+startHeader lvl attr = do
   whileM_ (headerLvl <&> (>= lvl)) popHeader
-  pushHeader lvl attr title
+  pushHeader lvl attr
 
 emptyAttr :: A.Attr
 emptyAttr = A.MkAttr "" [] M.empty
 
-run :: ParseM () -> Metadata -> [Block] -> Text -> A.Document
-run act mtdt bks title =
+run :: ParseM () -> Metadata -> [Block] -> A.Document
+run act mtdt bks =
   let doc =
         A.Document
           { A._docMtdt = mtdt,
@@ -102,59 +107,66 @@ run act mtdt bks title =
   where
     st =
       execState (act >> whileM_ (headerLvl <&> (> 0)) popHeader) $
-        ParseState bks (BSZ 0 emptyAttr title S.empty [] Nothing)
+        ParseState bks (BSZ 0 emptyAttr "" S.empty [] Nothing)
 
 parsePandoc :: Pandoc -> A.Document
-parsePandoc (Pandoc mtdt bks) =
-  run (whileJust_ getBlock parseTopBlock) (M.map toJSON $ unMeta mtdt) bks $
-    renderInlines $
-      parseInline =<< docTitle mtdt
+parsePandoc (Pandoc mtdt bks) = run act (M.map toJSON $ unMeta mtdt) bks
+  where
+    act = do
+      title <- concatMapM parseInline $ docTitle mtdt
+      stack . bszTitle .= renderInlines title
+      whileJust_ getBlock parseTopBlock
 
 parseTopBlock :: Block -> ParseM ()
-parseTopBlock (Header lvl attr title) = startHeader lvl parsed rendered
-  where
-    parsed = parseAttr attr
-    rendered = renderInlines $ parseInline =<< title
-parseTopBlock bk = mapM_ (pushBlock . noParent) $ parseBlock bk
+parseTopBlock (Header lvl attr title) = do
+  let parsed = parseAttr attr
+  startHeader lvl parsed
+  rendered <- renderInlines <$> concatMapM parseInline title
+  stack . bszTitle .= rendered
+parseTopBlock bk = mapM_ (pushBlock . noParent) =<< parseBlock bk
 
 -- Parse a block that is not a header
-parseBlock :: Block -> [A.Block]
-parseBlock (Plain inls) = pure $ A.Para $ parseInline =<< inls
-parseBlock (Para inls) = pure $ A.Para $ parseInline =<< inls
-parseBlock (LineBlock lns) = pure $ A.LineBlock $ (parseInline =<<) <$> lns
-parseBlock (CodeBlock attr txt) = pure $ A.CodeBlock (parseAttr attr) txt
-parseBlock (RawBlock _ _) = []
-parseBlock (BlockQuote bks) = pure $ A.BlockQuote $ parseBlock =<< bks
-parseBlock HorizontalRule = []
+parseBlock :: Block -> ParseM [A.Block]
+parseBlock (Plain inls) = pure . A.Para <$> concatMapM parseInline inls
+parseBlock (Para inls) = pure . A.Para <$> concatMapM parseInline inls
+parseBlock (LineBlock lns) = pure . A.LineBlock <$> mapM (concatMapM parseInline) lns
+parseBlock (CodeBlock attr txt) = pure . pure $ A.CodeBlock (parseAttr attr) txt
+parseBlock (RawBlock _ _) = pure []
+parseBlock (BlockQuote bks) = pure . A.BlockQuote <$> concatMapM parseBlock bks
+parseBlock HorizontalRule = pure []
 parseBlock (Table attr caption spec hd body ft) = undefined
 parseBlock (Figure attr caption bks) = undefined
-parseBlock _ = []
+parseBlock _ = pure []
 
-parseInline :: Inline -> [A.Inline]
-parseInline (Str txt) = pure $ A.Plain txt
-parseInline (Emph inls) = pure $ A.Styled A.Emph $ parseInline =<< inls
-parseInline (Underline inls) = parseInline =<< inls
-parseInline (Strong inls) = pure $ A.Styled A.Emph $ parseInline =<< inls
-parseInline (Strikeout inls) = parseInline =<< inls
-parseInline (Subscript inls) = parseInline =<< inls
-parseInline (Superscript inls) = parseInline =<< inls
-parseInline (SmallCaps inls) = parseInline =<< inls
-parseInline (Quoted _ inls) = pure $ A.Styled A.Quote $ parseInline =<< inls
-parseInline (Cite cts _) = A.Cite . MkId . citationId <$> cts
-parseInline (Code attr cd) = pure $ A.Code (parseAttr attr) cd
-parseInline Space = pure A.Space
-parseInline SoftBreak = pure A.Break
-parseInline LineBreak = pure A.Break
-parseInline (Math DisplayMath mth) = pure $ A.DisplayMath mth
-parseInline (Math InlineMath mth) = pure $ A.InlineMath mth
-parseInline (RawInline _ _) = []
+parseInline :: Inline -> ParseM [A.Inline]
+parseInline (Str txt) = pure . pure $ A.Plain txt
+parseInline (Emph inls) = pure . A.Styled A.Emph <$> concatMapM parseInline inls
+parseInline (Underline inls) = concatMapM parseInline inls
+parseInline (Strong inls) = pure . A.Styled A.Emph <$> concatMapM parseInline inls
+parseInline (Strikeout inls) = concatMapM parseInline inls
+parseInline (Subscript inls) = concatMapM parseInline inls
+parseInline (Superscript inls) = concatMapM parseInline inls
+parseInline (SmallCaps inls) = concatMapM parseInline inls
+parseInline (Quoted _ inls) = pure . A.Styled A.Quote <$> concatMapM parseInline inls
+parseInline (Cite cts _) = pure $ A.Cite . MkId . citationId <$> cts
+parseInline (Code attr cd) = pure . pure $ A.Code (parseAttr attr) cd
+parseInline Space = pure $ pure A.Space
+parseInline SoftBreak = pure $ pure A.Break
+parseInline LineBreak = pure $ pure A.Break
+parseInline (Math DisplayMath mth) = pure . pure $ A.DisplayMath mth
+parseInline (Math InlineMath mth) = pure . pure $ A.InlineMath mth
+parseInline (RawInline _ _) = pure []
 parseInline (Link attr txt (url, _)) =
   if T.isInfixOf "://" url
-    then pure $ A.PlainLink $ fromMaybe nullURI $ parseURI $ T.unpack url
-    else pure $ A.Link (parseAttr attr) (parseInline =<< txt) $ MkId url
+    then pure . pure $ A.PlainLink $ fromMaybe nullURI $ parseURI $ T.unpack url
+    else do
+      let i = MkId url
+      refTo i
+      t <- concatMapM parseInline txt
+      pure . pure $ A.Link (parseAttr attr) t i
 parseInline (Image attr txt url) = parseInline $ Link attr txt url
-parseInline (Note bks) = pure $ A.Sidenote $ parseBlock =<< bks
-parseInline (Span _ inls) = parseInline =<< inls
+parseInline (Note bks) = pure . A.Sidenote <$> concatMapM parseBlock bks
+parseInline (Span _ inls) = concatMapM parseInline inls
 
 parseAttr :: Attr -> A.Attr
 parseAttr (i, cls, mtdt) = A.MkAttr i cls $ M.fromList mtdt

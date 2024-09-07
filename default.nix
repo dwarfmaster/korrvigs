d0c150ba0660b9e6f66301dfd37915b9ac5fda64
DwarfMaster <luc@dwarfmaster.net>
2024-09-07 11:28:26 +0200

    Display points on leaflet widget

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
 { mkDerivation, aeson, array, async, base, base16-bytestring
-, base64, binary, bytestring, case-insensitive, clientsession
-, conduit, containers, data-default, data-endian, directory, extra
-, file-embed, filepath, hmatrix-glpk, http-conduit, http-types
-, HUnit, lens, lens-aeson, lib, linear, mime-types, monad-loops
-, mtl, network-uri, opaleye, optparse-applicative, pandoc
-, pandoc-types, parsec, parsec3-numbers, password
-, postgresql-simple, process, product-profunctors, profunctors
-, random, shakespeare, temporary, text, text-builder
-, text-manipulate, time, unix, utf8-string, vector, xdg-basedir
-, xml-conduit, yaml, yesod, yesod-core
+, base64, binary, blaze-html, bytestring, case-insensitive
+, clientsession, conduit, containers, data-default, data-endian
+, directory, extra, file-embed, filepath, hmatrix-glpk
+, http-conduit, http-types, HUnit, lens, lens-aeson, lib, linear
+, mime-types, monad-loops, mtl, network-uri, opaleye
+, optparse-applicative, pandoc, pandoc-types, parsec
+, parsec3-numbers, password, postgresql-simple, process
+, product-profunctors, profunctors, random, shakespeare, temporary
+, text, text-builder, text-manipulate, time, unix, utf8-string
+, vector, xdg-basedir, xml-conduit, yaml, yesod, yesod-core
 }:
 mkDerivation {
   pname = "korrvigs";
@@ -17,15 +17,15 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson array async base base16-bytestring base64 binary bytestring
-    case-insensitive clientsession conduit containers data-default
-    data-endian directory extra file-embed filepath hmatrix-glpk
-    http-conduit http-types HUnit lens lens-aeson linear mime-types
-    monad-loops mtl network-uri opaleye optparse-applicative pandoc
-    pandoc-types parsec parsec3-numbers password postgresql-simple
-    process product-profunctors profunctors random shakespeare
-    temporary text text-builder text-manipulate time unix utf8-string
-    vector xdg-basedir xml-conduit yaml yesod yesod-core
+    aeson array async base base16-bytestring base64 binary blaze-html
+    bytestring case-insensitive clientsession conduit containers
+    data-default data-endian directory extra file-embed filepath
+    hmatrix-glpk http-conduit http-types HUnit lens lens-aeson linear
+    mime-types monad-loops mtl network-uri opaleye optparse-applicative
+    pandoc pandoc-types parsec parsec3-numbers password
+    postgresql-simple process product-profunctors profunctors random
+    shakespeare temporary text text-builder text-manipulate time unix
+    utf8-string vector xdg-basedir xml-conduit yaml yesod yesod-core
   ];
   testHaskellDepends = [ base HUnit ];
   doHaddock = false;
--- lib/Korrvigs/Web/Leaflet.hs
+++ lib/Korrvigs/Web/Leaflet.hs
@@ -1,4 +1,4 @@
-module Korrvigs.Web.Leaflet where
+module Korrvigs.Web.Leaflet (MapItem (..), leafletWidget) where
 
 import Control.Lens
 import Control.Monad
@@ -7,17 +7,33 @@ import Data.Text (Text)
 import Korrvigs.Geometry
 import Korrvigs.Web.Backend
 import qualified Korrvigs.Web.Ressources as Rcs
+import Linear.V2
+import Text.Blaze.Html.Renderer.Text
+import Text.Julius
 import Yesod
 
 data MapItem = MapItem
   { _mitGeo :: Geometry,
-    _mitContent :: Maybe Html
+    _mitContent :: Maybe ((Route WebData -> Text) -> Html)
   }
 
 makeLenses ''MapItem
 
+points :: Geometry -> [V2 Double]
+points (GeoPoint pt) = [pt]
+points (GeoPath path) = path
+points (GeoPolygon (Polygon path _)) = path
+
+computeCenter :: [Geometry] -> Point
+computeCenter geoms = sum pts / V2 npts npts
+  where
+    pts :: [V2 Double]
+    pts = concatMap points geoms
+    npts :: Double
+    npts = fromIntegral $ length pts
+
 leafletWidget :: Maybe Text -> [MapItem] -> Widget
-leafletWidget details _ = do
+leafletWidget details items = do
   Rcs.leaflet
   detClass <- newIdent
   case details of
@@ -28,14 +44,33 @@ leafletWidget details _ = do
         <div #map>
       |]
     Nothing -> [whamlet|<div #map>|]
+  let (V2 centerY centerX) = computeCenter $ (^. mitGeo) <$> items
   toWidget
     [julius|
-    var map = L.map('map').setView([51.505, -0.09], 13)
+    var map = L.map('map').setView([#{rawJS $ show centerX}, #{rawJS $ show centerY}], 13)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
   |]
+  render <- getUrlRender
+  forM_ items $ \item -> do
+    markerVar <- newIdent
+    case item ^. mitGeo of
+      GeoPoint (V2 y x) ->
+        toWidget
+          [julius|
+          var #{rawJS markerVar} = L.marker([#{rawJS $ show x}, #{rawJS $ show y}]).addTo(map)
+        |]
+      GeoPath path -> undefined
+      GeoPolygon poly -> undefined
+    case item ^. mitContent of
+      Nothing -> pure ()
+      Just content ->
+        toWidget
+          [julius|
+          #{rawJS markerVar}.bindPopup(#{renderHtml $ content render})
+        |]
   when (isJust details) $
     toWidget
       [julius|

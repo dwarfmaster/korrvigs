90d59eb3d7403394e55871f90d1b69c81c775d78
DwarfMaster <luc@dwarfmaster.net>
2024-12-21 18:08:59 +0100

    Display git status as a tree in the web page

--- lib/Korrvigs/Web/Git.hs
+++ lib/Korrvigs/Web/Git.hs
@@ -1,50 +1,102 @@
 module Korrvigs.Web.Git (getGitR, postGitR) where
 
 import Control.Lens
+import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Korrvigs.Utils.Git.Status as St
 import Korrvigs.Web.Backend
 import Korrvigs.Web.Login (logWrap)
+import System.FilePath
 import Yesod
 
-renderStatus :: St.FileStatus -> Widget
-renderStatus (St.StatusChanged change) =
+data GitStatus
+  = GitNewFile
+  | GitDeletedFile
+  | GitChangedFile
+
+data FileTree
+  = FileTreeDir (Map Text FileTree)
+  | FileTreeFile FilePath GitStatus
+
+emptyFileTree :: FileTree
+emptyFileTree = FileTreeDir M.empty
+
+singletonFileTree :: FilePath -> GitStatus -> [Text] -> FileTree
+singletonFileTree path st [] = FileTreeFile path st
+singletonFileTree path st (part : prts) = FileTreeDir $ M.singleton part $ singletonFileTree path st prts
+
+insertIntoFileTree' :: FilePath -> GitStatus -> [Text] -> FileTree -> FileTree
+insertIntoFileTree' _ _ _ (FileTreeFile _ _) = error "Invalid insertion into filetree : inserting structure onto already existing file"
+insertIntoFileTree' _ _ [] _ = error "Invalid insertion into filetree : inserting file onto already existing structure"
+insertIntoFileTree' path st (part : prts) (FileTreeDir subs) = case M.lookup part subs of
+  Nothing -> FileTreeDir $ M.insert part (singletonFileTree path st prts) subs
+  Just ft -> FileTreeDir $ M.insert part (insertIntoFileTree' path st prts ft) subs
+
+insertIntoFileTree :: FilePath -> GitStatus -> FileTree -> FileTree
+insertIntoFileTree path st = insertIntoFileTree' path st comps
+  where
+    comps :: [Text]
+    comps = T.pack <$> splitDirectories path
+
+extractGitStatus :: St.FileStatus -> [(FilePath, GitStatus)]
+extractGitStatus (St.StatusChanged change) = [(change ^. St.changePath, nstatus)]
+  where
+    nstatus = case (change ^. St.changeIndex, change ^. St.changeTree) of
+      (St.Deleted, St.Added) -> GitChangedFile
+      (St.Added, _) -> GitNewFile
+      (_, St.Added) -> GitNewFile
+      (St.Deleted, _) -> GitDeletedFile
+      (_, St.Deleted) -> GitDeletedFile
+      _ -> GitChangedFile
+extractGitStatus (St.StatusMoved move) =
+  [ (move ^. St.moveSource, GitDeletedFile),
+    (move ^. St.moveTarget, GitNewFile)
+  ]
+extractGitStatus (St.StatusUnknown path) = [(path, GitNewFile)]
+extractGitStatus (St.StatusIgnored _) = []
+
+gitFileTree :: [St.FileStatus] -> FileTree
+gitFileTree statuses = foldr (uncurry insertIntoFileTree) emptyFileTree $ extractGitStatus =<< statuses
+
+renderSubTree :: Text -> FileTree -> Widget
+renderSubTree sub ft@(FileTreeDir _) =
   [whamlet|
-    <details>
-      <summary>#{path} modified
-      File changed!
+    <tt>#{sub}
+    ^{renderFileTree ft}
+  |]
+renderSubTree sub (FileTreeFile _ status) =
+  [whamlet|
+    #{sym}
+    <tt>#{sub}
   |]
   where
-    path = change ^. St.changePath
-renderStatus (St.StatusMoved move) =
+    sym :: Text
+    sym = case status of
+      GitNewFile -> "⊕"
+      GitDeletedFile -> "⊖"
+      GitChangedFile -> ""
+
+renderFileTree :: FileTree -> Widget
+renderFileTree (FileTreeFile _ _) = mempty
+renderFileTree (FileTreeDir subs) =
   [whamlet|
-    <details>
-      <summary>#{source} #{o} #{target}
-      Score: #{sc}
+    <ul>
+      $forall (path,sub) <- M.toList subs
+        <li>
+          ^{renderSubTree path sub}
   |]
-  where
-    source = move ^. St.moveSource
-    target = move ^. St.moveTarget
-    (o, sc) = case move ^. St.moveScore of
-      St.CopiedScore score -> ("->>" :: Text, score)
-      St.MovedScore score -> ("->", score)
-renderStatus (St.StatusUnknown path) =
-  [whamlet|
-    <details>
-      <summary>#{path} created
-      New file!
-    |]
-renderStatus (St.StatusIgnored _) = mempty
 
 getGitR :: Handler Html
 getGitR = do
   statuses <- St.gitStatusKorr
+  let ft = gitFileTree statuses
   logWrap $
     defaultLayout
       [whamlet|
   <h1>Status
-  $forall st <- statuses
-    ^{renderStatus st}
+  ^{renderFileTree ft}
   |]
 
 postGitR :: Handler Html

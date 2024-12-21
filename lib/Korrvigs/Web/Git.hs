0dc419107aa3b7276051f1155f26dd5bc208cab9
DwarfMaster <luc@dwarfmaster.net>
2024-12-21 18:43:17 +0100

    Flatten singleton directories in git status web view

--- lib/Korrvigs/Web/Git.hs
+++ lib/Korrvigs/Web/Git.hs
@@ -4,12 +4,11 @@ import Control.Lens
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Text (Text)
-import qualified Data.Text as T
 import qualified Korrvigs.Utils.Git.Status as St
 import Korrvigs.Web.Backend
 import Korrvigs.Web.Login (logWrap)
 import System.FilePath
-import Yesod
+import Yesod hiding (joinPath)
 
 data GitStatus
   = GitNewFile
@@ -17,17 +16,17 @@ data GitStatus
   | GitChangedFile
 
 data FileTree
-  = FileTreeDir (Map Text FileTree)
+  = FileTreeDir (Map FilePath FileTree)
   | FileTreeFile FilePath GitStatus
 
 emptyFileTree :: FileTree
 emptyFileTree = FileTreeDir M.empty
 
-singletonFileTree :: FilePath -> GitStatus -> [Text] -> FileTree
+singletonFileTree :: FilePath -> GitStatus -> [FilePath] -> FileTree
 singletonFileTree path st [] = FileTreeFile path st
 singletonFileTree path st (part : prts) = FileTreeDir $ M.singleton part $ singletonFileTree path st prts
 
-insertIntoFileTree' :: FilePath -> GitStatus -> [Text] -> FileTree -> FileTree
+insertIntoFileTree' :: FilePath -> GitStatus -> [FilePath] -> FileTree -> FileTree
 insertIntoFileTree' _ _ _ (FileTreeFile _ _) = error "Invalid insertion into filetree : inserting structure onto already existing file"
 insertIntoFileTree' _ _ [] _ = error "Invalid insertion into filetree : inserting file onto already existing structure"
 insertIntoFileTree' path st (part : prts) (FileTreeDir subs) = case M.lookup part subs of
@@ -37,8 +36,8 @@ insertIntoFileTree' path st (part : prts) (FileTreeDir subs) = case M.lookup par
 insertIntoFileTree :: FilePath -> GitStatus -> FileTree -> FileTree
 insertIntoFileTree path st = insertIntoFileTree' path st comps
   where
-    comps :: [Text]
-    comps = T.pack <$> splitDirectories path
+    comps :: [FilePath]
+    comps = splitDirectories path
 
 extractGitStatus :: St.FileStatus -> [(FilePath, GitStatus)]
 extractGitStatus (St.StatusChanged change) = [(change ^. St.changePath, nstatus)]
@@ -57,10 +56,19 @@ extractGitStatus (St.StatusMoved move) =
 extractGitStatus (St.StatusUnknown path) = [(path, GitNewFile)]
 extractGitStatus (St.StatusIgnored _) = []
 
-gitFileTree :: [St.FileStatus] -> FileTree
-gitFileTree statuses = foldr (uncurry insertIntoFileTree) emptyFileTree $ extractGitStatus =<< statuses
+simplifyFileTree' :: FilePath -> FileTree -> (FilePath, FileTree)
+simplifyFileTree' path ft@(FileTreeFile _ _) = (path, ft)
+simplifyFileTree' path (FileTreeDir subs) = case M.toList subs of
+  [(sub, subv)] -> let (nsub, nft) = simplifyFileTree' sub subv in (joinPath [path, nsub], nft)
+  lsubs -> (path, FileTreeDir $ M.fromList $ uncurry simplifyFileTree' <$> lsubs)
 
-renderSubTree :: Text -> FileTree -> Widget
+simplifyFileTree :: FileTree -> FileTree
+simplifyFileTree = snd . simplifyFileTree' ""
+
+gitFileTree :: [St.FileStatus] -> FileTree
+gitFileTree statuses = simplifyFileTree $ foldr (uncurry insertIntoFileTree) emptyFileTree $ extractGitStatus =<< statuses
+
+renderSubTree :: FilePath -> FileTree -> Widget
 renderSubTree sub ft@(FileTreeDir _) =
   [whamlet|
     <tt>#{sub}

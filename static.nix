2e3c8feb5e9190d62ca22782a97a593bde795395
DwarfMaster <luc@dwarfmaster.net>
2024-12-30 09:56:43 +0100

    Add ace js editor to static files

--- static.nix
+++ static.nix
@@ -3,6 +3,7 @@
   nodePackages_latest,
   fetchzip,
   fetchurl,
+  fetchFromGitHub,
 }: let
   leaflet = fetchzip {
     url = "https://leafletjs-cdn.s3.amazonaws.com/content/leaflet/v1.9.4/leaflet.zip";
@@ -17,6 +18,12 @@
     url = "https://unpkg.com/vis-timeline/standalone/umd/vis-timeline-graph2d.min.js";
     sha256 = "1mk5x9fjnppv5l42m225qnplymbxn3565rn572jrcdhhaxsn5xam";
   };
+  ace = fetchFromGitHub {
+    owner = "ajaxorg";
+    repo = "ace-builds";
+    rev = "da6219a75f73d63e2298c9b4c293793faf98e6d9";
+    sha256 = "1pbfz7bqrpliwc2n0lzmssmpykh5qszvmfr6q149syvvdznklhd3";
+  };
 in
   runCommand "korrvigs-static" {} ''
     mkdir -p $out
@@ -25,4 +32,5 @@ in
     mkdir -p $out/vis
     cp ${vis-network} $out/vis/vis-network.min.js
     cp ${vis-timeline} $out/vis/vis-timeline-graph2d.min.js
+    cp -r ${ace}/src-min $out/ace
   ''

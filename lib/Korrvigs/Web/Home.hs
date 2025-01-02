c92d3b9be4e3ead0f3b2b13923b4ae34d71e7b9e
DwarfMaster <luc@dwarfmaster.net>
2025-01-02 22:24:41 +0100

    Add form to create new links from web

--- lib/Korrvigs/Web/Home.hs
+++ lib/Korrvigs/Web/Home.hs
@@ -2,6 +2,7 @@ module Korrvigs.Web.Home (getHomeR, postHomeR) where
 
 import Control.Lens
 import Data.Text (Text)
+import qualified Korrvigs.Link.New as NLink
 import qualified Korrvigs.Note.New as NNote
 import Korrvigs.Web.Backend
 import Korrvigs.Web.Login (logWrap)
@@ -12,30 +13,55 @@ import Prelude hiding (not)
 
 newtype NewNote = NewNote {_nnoteTitle :: Text}
 
+data NewLink = NewLink {_nlinkTitle :: Maybe Text, _nlinkUrl :: Text}
+
 makeLenses ''NewNote
+makeLenses ''NewLink
 
 newNoteForm :: Html -> MForm Handler (FormResult NewNote, Widget)
 newNoteForm = renderDivs $ NewNote <$> areq textField "Title" Nothing
 
+newLinkForm :: Html -> MForm Handler (FormResult NewLink, Widget)
+newLinkForm =
+  renderDivs $
+    NewLink
+      <$> aopt textField "Title" Nothing
+      <*> areq textField "URL" Nothing
+
+renderForm :: Text -> (Html -> MForm Handler (FormResult a, Widget)) -> Handler Widget
+renderForm title form = do
+  (widget, enctype) <- generateFormPost form
+  pure
+    [whamlet|
+    <details .search-group>
+      <summary>
+        #{title}
+      <form method=post action=@{HomeR} enctype=#{enctype}>
+        ^{widget}
+        <button>
+          #{title}
+  |]
+
 getHomeR :: Handler Html
 getHomeR = do
-  (nnoteW, nnoteEnctype) <- generateFormPost newNoteForm
+  newNote <- renderForm "Create new note" newNoteForm
+  newLink <- renderForm "Create new link" newLinkForm
   logWrap $
     defaultLayout $ do
       Rcs.formsStyle
       [whamlet|
       <h1>Welcome to Korrvigs
-      <details .search-group>
-        <summary>Create new note
-        <form method=post action=@{HomeR} enctype=#{nnoteEnctype}>
-          ^{nnoteW}
-          <button>New note
+      ^{newNote}
+      ^{newLink}
     |]
 
 postHomeR :: Handler Html
-postHomeR = runNewNote
+postHomeR = do
+  runNewNote
+  runNewLink
+  getHomeR
 
-runNewNote :: Handler Html
+runNewNote :: Handler ()
 runNewNote = do
   ((result, _), _) <- runFormPost newNoteForm
   case result of
@@ -43,4 +69,20 @@ runNewNote = do
       let settings = NNote.NewNote (nnote ^. nnoteTitle) Nothing
       i <- NNote.new settings
       redirect $ EntryR $ WId i
-    _ -> getHomeR
+    _ -> pure ()
+
+runNewLink :: Handler ()
+runNewLink = do
+  ((result, _), _) <- runFormPost newLinkForm
+  case result of
+    FormSuccess nlink | nlink ^. nlinkUrl /= "" -> do
+      let settings =
+            NLink.NewLink
+              { NLink._nlOffline = False,
+                NLink._nlDate = Nothing,
+                NLink._nlTitle = nlink ^. nlinkTitle,
+                NLink._nlParent = Nothing
+              }
+      i <- NLink.new (nlink ^. nlinkUrl) settings
+      redirect $ EntryR $ WId i
+    _ -> pure ()

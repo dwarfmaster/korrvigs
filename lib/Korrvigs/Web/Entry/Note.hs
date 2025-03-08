9255527e7ea1d7769aa3324357661f57e43c76c0
DwarfMaster <luc@dwarfmaster.net>
2025-03-08 01:04:02 +0100

    Fix generation of paragraph html

--- lib/Korrvigs/Web/Entry/Note.hs
+++ lib/Korrvigs/Web/Entry/Note.hs
@@ -8,6 +8,7 @@ import Data.Array
 import Data.List
 import qualified Data.Map as M
 import Data.Maybe
+import Data.Monoid
 import Data.Text (Text)
 import qualified Data.Text as T
 import Korrvigs.Entry
@@ -23,6 +24,10 @@ import qualified Korrvigs.Web.Entry.Link as Link
 import qualified Korrvigs.Web.Ressources as Rcs
 import Korrvigs.Web.Routes
 import qualified Korrvigs.Web.Widgets as Wdgs
+import Text.Blaze hiding ((!))
+import qualified Text.Blaze as Blz
+import qualified Text.Blaze.Html5 as Html
+import qualified Text.Blaze.Html5.Attributes as Attr
 import Yesod hiding (Attr, get, (.=))
 import Yesod.Static hiding (embed)
 
@@ -314,6 +319,16 @@ compileAttrWithClasses cls attr =
 compileAttr :: Attr -> [(Text, Text)]
 compileAttr = compileAttrWithClasses []
 
+applyAttr :: Attribute -> Html -> Html
+applyAttr attr html = html Blz.! attr
+
+compileAttr' :: Attr -> Html -> Html
+compileAttr' (MkAttr i clss misc) = applyId . applyClasses . applyMisc
+  where
+    applyId = if T.null i then id else applyAttr $ Attr.id $ textValue i
+    applyClasses = if null clss then id else applyAttr $ Attr.class_ $ textValue $ T.intercalate " " clss
+    applyMisc = appEndo $ foldMap (\(k, v) -> Endo $ applyAttr $ customAttribute (textTag k) $ textValue v) $ M.toList misc
+
 compileHead :: Id -> Int -> Maybe Text -> Text -> Text -> Maybe Task -> Checks -> SubLoc -> Handler Widget
 compileHead entry n hdId t edit task checks subL = do
   btm <- editButton entry (min n 5) hdId edit subL
@@ -426,40 +441,56 @@ compileTableRow isHeader cells row =
     ncols = bounds cells ^. _2 . _1
 
 compileInlines :: [Inline] -> CompileM Widget
-compileInlines = fmap mconcat . mapM compileInline
+compileInlines inls = do
+  (c, w) <- compileInlines' inls
+  pure $ do
+    w
+    toWidget c
 
-compileInline :: Inline -> CompileM Widget
-compileInline (Plain txt) = pure $ toWidget txt
+compileInlines' :: [Inline] -> CompileM (Html, Widget)
+compileInlines' = foldM (\w i -> sidecat w <$> compileInline i) mempty
+  where
+    sidecat :: (Html, Widget) -> (Html, Widget) -> (Html, Widget)
+    sidecat (c1, w1) (c2, w2) = (c1 <> c2, w1 <> w2)
+
+compileInline :: Inline -> CompileM (Html, Widget)
+compileInline (Plain txt) = pure (toMarkup txt, mempty)
 compileInline (Styled Emph inls) = do
-  inlsW <- compileInlines inls
-  pure [whamlet|<em>^{inlsW}|]
+  (inlsH, inlsW) <- compileInlines' inls
+  pure (Html.em inlsH, inlsW)
 compileInline (Styled Quote inls) = do
-  inlsW <- compileInlines inls
-  pure [whamlet|<q>^{inlsW}|]
+  (inlsH, inlsW) <- compileInlines' inls
+  pure (Html.q inlsH, inlsW)
 compileInline (Styled SubScript inls) = do
-  inlsW <- compileInlines inls
-  pure [whamlet|<sub>^{inlsW}|]
+  (inlsH, inlsW) <- compileInlines' inls
+  pure (Html.sub inlsH, inlsW)
 compileInline (Styled SuperScript inls) = do
-  inlsW <- compileInlines inls
-  pure [whamlet|<sup>^{inlsW}|]
-compileInline (Code attr txt) = pure [whamlet|<code *{compileAttr attr}>#{txt}|]
+  (inlsH, inlsW) <- compileInlines' inls
+  pure (Html.sup inlsH, inlsW)
+compileInline (Code attr txt) =
+  pure (compileAttr' attr $ Html.code $ toMarkup txt, mempty)
 compileInline (Link attr inls tgt) = do
-  inlsW <- compileInlines inls
-  pure [whamlet|<a href=@{EntryR $ WId tgt} *{compileAttr attr}>^{inlsW}|]
-compileInline (Cite i) = pure [whamlet|<cite><a href=@{EntryR $ WId i}>@#{unId i}|]
+  (inlsH, inlsW) <- compileInlines' inls
+  render <- getUrlRender
+  let link = textValue $ render $ EntryR $ WId tgt
+  pure (applyAttr (Attr.href link) $ compileAttr' attr $ Html.a inlsH, inlsW)
+compileInline (Cite i) = do
+  render <- getUrlRender
+  let link = textValue $ render $ EntryR $ WId i
+  pure (applyAttr (Attr.href link) $ Html.a $ toMarkup $ unId i, mempty)
 compileInline (PlainLink mtitle uri) = do
-  let url = show uri
-  title <- case mtitle of
-    Just t -> compileInlines t
-    Nothing -> pure [whamlet|#{url}|]
-  pure [whamlet|<a href=#{url}>^{title}|]
-compileInline Space = pure $ toWidget (" " :: Text)
-compileInline Break = pure [whamlet|<br>|]
-compileInline (DisplayMath mth) = pure . toWidget . toHtml $ "$$" <> mth <> "$$"
-compileInline (InlineMath mth) = pure . toWidget . toHtml $ "\\(" <> mth <> "\\)"
+  let url = T.pack $ show uri
+  (titleH, titleW) <- case mtitle of
+    Just t -> compileInlines' t
+    Nothing -> pure (toMarkup url, mempty)
+  pure (applyAttr (Attr.href $ textValue url) $ Html.a titleH, titleW)
+compileInline Space = pure (toMarkup (" " :: Text), mempty)
+compileInline Break = pure (Html.br, mempty)
+compileInline (DisplayMath mth) = pure (toMarkup $ "$$" <> mth <> "$$", mempty)
+compileInline (InlineMath mth) = pure (toMarkup $ "\\(" <> mth <> "\\)", mempty)
 compileInline (Sidenote side) = do
   note <- pushSide side
-  pure [whamlet|<span .sidenote-ref>#{show note}|]
+  pure (applyAttr (Attr.class_ "sidenote-ref") $ Html.span $ toMarkup $ show note, mempty)
 compileInline (Check ck) = do
   render <- getUrlRender
   loc <- CheckLoc <$> use subLoc <*> use checkboxCount
@@ -472,9 +503,12 @@ compileInline (Check ck) = do
   let postUrl = render $ NoteSubR (WId entry) $ WLoc $ LocCheck loc
   checkboxCount += 1
   cid <- newIdent
-  pure $ do
-    toWidget [julius|setupCheckbox(#{postUrl}, #{todoUrl}, #{ongoingUrl}, #{blockedUrl}, #{doneUrl}, #{dontUrl}, #{cid});|]
-    [whamlet|<img ##{cid} src=@{checkImg ck} .checkBox>|]
+  let w = toWidget [julius|setupCheckbox(#{postUrl}, #{todoUrl}, #{ongoingUrl}, #{blockedUrl}, #{doneUrl}, #{dontUrl}, #{cid});|]
+  let h =
+        applyAttr (Attr.id $ textValue cid) $
+          applyAttr (Attr.src $ textValue $ render $ checkImg ck) $
+            applyAttr (Attr.class_ "checkBox") Html.img
+  pure (h, w)
 
 checkImg :: TaskStatus -> Route WebData
 checkImg TaskTodo = StaticR $ StaticRoute ["icons", "checkbox-todo.svg"] []

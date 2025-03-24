d0b1cdfca0cae0d35abc42cefa831c29ad830992
DwarfMaster <luc@dwarfmaster.net>
2025-03-24 23:28:44 +0100

    Import metadata from OpenLibrary

---
+++ lib/Korrvigs/Metadata/Media/OpenLibrary.hs
@@ -0,0 +1,140 @@
+module Korrvigs.Metadata.Media.OpenLibrary (OpenLibraryQuery (..), queryOpenLibrary) where
+
+import Citeproc.Types (readAsInt)
+import Conduit (runResourceT)
+import Control.Lens
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Conduit
+import Data.Conduit.Combinators (sinkLazy)
+import Data.Foldable (toList)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Time.Calendar
+import Korrvigs.Metadata.Media.Ontology
+import Network.HTTP.Conduit
+import Network.HTTP.Types.Status
+import Network.URI
+import System.FilePath
+
+data OpenLibraryQuery
+  = OLISBN Text
+  | OLKey Text
+  | OLUrl Text
+  deriving (Ord, Eq, Show)
+
+data OLResult = OLResult
+  { _olUrl :: Text,
+    _olTitle :: Text,
+    _olISBN10 :: [Text],
+    _olISBN13 :: [Text],
+    _olPublisher :: [Text],
+    _olAuthors :: [Text],
+    _olPublishDate :: Text,
+    _olDescription :: Text,
+    _olSeries :: [Text]
+  }
+  deriving (Ord, Eq, Show)
+
+makeLenses ''OLResult
+
+parseAuthors :: Value -> Parser [Text]
+parseAuthors =
+  withArray "OLResult authors" $
+    fmap toList . mapM (withObject "OLResult author" $ \obj -> obj .: "name")
+
+instance FromJSON OLResult where
+  parseJSON = withObject "OLResult" $ \obj ->
+    OLResult
+      <$> obj .: "info_url"
+      <*> ((obj .: "details") >>= (.: "title"))
+      <*> ((obj .: "details") >>= (.: "isbn_10"))
+      <*> ((obj .: "details") >>= (.: "isbn_13"))
+      <*> ((obj .: "details") >>= (.: "publishers"))
+      <*> ((obj .: "details") >>= (.: "authors") >>= parseAuthors)
+      <*> ((obj .: "details") >>= (.: "publish_date"))
+      <*> ((obj .: "details") >>= (.: "description"))
+      <*> ((obj .: "details") >>= (.: "series"))
+
+openUrl :: Text
+openUrl = "https://openlibrary.org"
+
+mkAPIBaseUrl :: OpenLibraryQuery -> Maybe Text
+mkAPIBaseUrl (OLISBN isbn) = Just $ openUrl <> "/api/books?bibkeys=ISBN:" <> isbn
+mkAPIBaseUrl (OLKey key) = Just $ openUrl <> "/api/books?bibkeys=OLID:" <> key
+mkAPIBaseUrl (OLUrl url) =
+  case parseURI (T.unpack url) >>= (^? ix 2) . splitPath . uriPath of
+    Just keyWithSlash -> mkAPIBaseUrl $ OLKey $ T.pack $ init keyWithSlash
+    Nothing -> Nothing
+
+mkAPIUrl :: OpenLibraryQuery -> Maybe Text
+mkAPIUrl = fmap (<> "&format=json&jscmd=details") . mkAPIBaseUrl
+
+parsePublishMonth :: Text -> Maybe MonthOfYear
+parsePublishMonth t
+  | T.isPrefixOf "January" t || T.isPrefixOf "Jan" t = Just January
+  | T.isPrefixOf "February" t || T.isPrefixOf "Feb" t = Just February
+  | T.isPrefixOf "March" t || T.isPrefixOf "Mar" t = Just March
+  | T.isPrefixOf "April" t || T.isPrefixOf "Apr" t = Just April
+  | T.isPrefixOf "May" t || T.isPrefixOf "May" t = Just May
+  | T.isPrefixOf "June" t || T.isPrefixOf "Jun" t = Just June
+  | T.isPrefixOf "July" t || T.isPrefixOf "Jul" t = Just July
+  | T.isPrefixOf "August" t || T.isPrefixOf "Aug" t = Just August
+  | T.isPrefixOf "September" t || T.isPrefixOf "Sep" t = Just September
+  | T.isPrefixOf "October" t || T.isPrefixOf "Oct" t = Just October
+  | T.isPrefixOf "November" t || T.isPrefixOf "Nov" t = Just November
+  | T.isPrefixOf "December" t || T.isPrefixOf "Dec" t = Just December
+  | otherwise = Nothing
+
+parsePublishYear :: Text -> Maybe Year
+parsePublishYear t = toInteger <$> readAsInt yr
+  where
+    yr = T.takeEnd 4 t
+
+queryOpenLibrary :: OpenLibraryQuery -> IO (Maybe Media)
+queryOpenLibrary q = case mkAPIUrl q of
+  Nothing -> pure Nothing
+  Just url -> do
+    req <- parseRequest $ T.unpack url
+    man <- newManager tlsManagerSettings
+    content <- runResourceT $ do
+      resp <- http req man
+      let scode = statusCode (responseStatus resp)
+      if scode == 200
+        then fmap Just $ runConduit $ responseBody resp .| sinkLazy
+        else pure Nothing
+    pure $ case eitherDecode <$> content of
+      Nothing -> Nothing
+      Just (Left _) -> Nothing
+      Just (Right (olmap :: Map Text OLResult)) ->
+        let olr = head $ M.elems olmap
+         in Just $
+              Media
+                { _medType = Book,
+                  _medAbstract = Just $ T.replace "\r\n" "\n" $ olr ^. olDescription,
+                  _medBibtex = Nothing,
+                  _medDOI = [],
+                  _medISBN = olr ^. olISBN10 <> olr ^. olISBN13,
+                  _medISSN = [],
+                  _medTitle = Just $ olr ^. olTitle,
+                  _medAuthors = olr ^. olAuthors,
+                  _medMonth = parsePublishMonth $ olr ^. olPublishDate,
+                  _medYear = parsePublishYear $ olr ^. olPublishDate,
+                  _medUrl = Just $ olr ^. olUrl,
+                  _medRSS = Nothing,
+                  _medSource = [],
+                  _medPublisher = olr ^. olPublisher,
+                  _medContainer =
+                    listToMaybe $
+                      MediaContainer
+                        <$> olr ^. olSeries
+                        <*> pure Nothing
+                        <*> pure Nothing
+                        <*> pure Nothing
+                        <*> pure Nothing,
+                  _medInstitution = [],
+                  _medLicense = []
+                }

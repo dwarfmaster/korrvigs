340265e19182396376c21b50810bc83a87be48a3
DwarfMaster <luc@dwarfmaster.net>
2025-07-20 13:10:52 +0200

    When a links has a cover, display it instead of an iframe

--- lib/Korrvigs/Web/Entry/Link.hs
+++ lib/Korrvigs/Web/Entry/Link.hs
@@ -2,17 +2,28 @@ module Korrvigs.Web.Entry.Link (content, embed) where
 
 import Control.Lens
 import Korrvigs.Entry
+import Korrvigs.Metadata
+import Korrvigs.Metadata.Media
 import Korrvigs.Web.Backend
+import Korrvigs.Web.Routes
 import Yesod
 
 embed :: Int -> Link -> Handler Widget
 embed lvl link =
-  pure
-    [whamlet|
-    <a href=#{ref}>#{ref}
-    $if lvl == 0
-      <iframe width=100% height=700 src=#{ref}>
-  |]
+  rSelectMtdt Cover (sqlId $ link ^. linkEntry . name) >>= \case
+    Just cid ->
+      pure
+        [whamlet|
+      <a href=#{ref}>
+        <img src=@{EntryDownloadR $ WId $ MkId cid} style="width: 100%">
+    |]
+    Nothing ->
+      pure
+        [whamlet|
+      <a href=#{ref}>#{ref}
+      $if lvl == 0
+        <iframe width=100% height=700 src=#{ref}>
+    |]
   where
     ref = link ^. linkRef
 

41ad5ad5f390f6a67bc79d9099b79585a45d6273
DwarfMaster <luc@dwarfmaster.net>
2025-12-06 15:00:22 +0100

    Implement basic ERIS decoding

--- lib/Data/ERIS.hs
+++ lib/Data/ERIS.hs
@@ -1,1 +1,16 @@
-module Data.ERIS where
+module Data.ERIS
+  ( module Data.ERIS.Crypto,
+    ConvergenceSecret,
+    erisEncode,
+    erisDecode,
+    module Data.ERIS.DB.Class,
+    ERISFileDB (..),
+    dbRoot,
+  )
+where
+
+import Data.ERIS.Crypto
+import Data.ERIS.DB.Class
+import Data.ERIS.DB.Plain
+import Data.ERIS.Decode (erisDecode)
+import Data.ERIS.Encode (ConvergenceSecret, erisEncode)
---
+++ lib/Data/ERIS/Decode.hs
@@ -0,0 +1,75 @@
+module Data.ERIS.Decode where
+
+import Control.Lens
+import Control.Monad
+import Crypto.Hash
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Builder as Bld
+import qualified Data.ByteString.Lazy as LBS
+import Data.ERIS.Crypto
+import Data.ERIS.DB.Class
+import Data.List (unfoldr)
+import Data.Word (Word8)
+
+erisDecodeRecurse ::
+  forall db m.
+  (ERISBlockRead db m, MonadFail m) =>
+  db ->
+  Int ->
+  Word8 ->
+  ERISHash ->
+  ERISHash ->
+  m Bld.Builder
+erisDecodeRecurse db blockSize level reference key = do
+  node <- erisDereferenceNode db reference key level blockSize
+  if level == 0
+    then pure $ Bld.byteString node
+    else do
+      subNodes <-
+        forM (refKeyPairs node) $
+          uncurry $
+            erisDecodeRecurse db blockSize (level - 1)
+      pure $ mconcat subNodes
+  where
+    decodeRefKey :: ByteString -> Maybe (ERISHash, ERISHash)
+    decodeRefKey bs | BS.all (== 0x00) bs = Nothing
+    decodeRefKey bs = do
+      let (ref, ky) = BS.splitAt 32 bs
+      hashRef <- digestFromByteString ref
+      hashKey <- digestFromByteString ky
+      pure (hashRef, hashKey)
+    decodeOneRefKey :: ByteString -> Maybe ((ERISHash, ERISHash), ByteString)
+    decodeOneRefKey bs | BS.null bs = Nothing
+    decodeOneRefKey bs = do
+      let (pair, rest) = BS.splitAt 64 bs
+      (ref, ky) <- decodeRefKey pair
+      pure ((ref, ky), rest)
+    refKeyPairs :: ByteString -> [(ERISHash, ERISHash)]
+    refKeyPairs = unfoldr decodeOneRefKey
+
+erisDecode :: (ERISBlockRead db m, MonadFail m) => db -> ERISCapability -> m ByteString
+erisDecode db cap = do
+  when (level > 0) $ erisVerifyKey db level rootReference rootKey blockSize
+  padded <- erisDecodeRecurse db blockSize level rootReference rootKey
+  pure $ erisUnpad (LBS.toStrict $ Bld.toLazyByteString padded) blockSize
+  where
+    level = cap ^. erisCapLevel
+    blockSize = cap ^. erisCapBlockSize
+    rootReference = cap ^. erisCapRootRef
+    rootKey = cap ^. erisCapRootKey
+
+erisDereferenceNode :: (ERISBlockRead db m, MonadFail m) => db -> ERISHash -> ERISHash -> Word8 -> Int -> m ByteString
+erisDereferenceNode db reference key level blockSize = do
+  block <- erisBlockStorageGet db reference >>= maybe (fail "block not in database") pure
+  when (BS.length block /= blockSize) $ fail "block has invalid size"
+  when (erisBlake2b block /= reference) $ fail "block is invalid"
+  let nonce = mkErisNonceNum level
+  pure $ erisChaCha20 block key nonce
+
+erisVerifyKey :: (ERISBlockRead db m, MonadFail m) => db -> Word8 -> ERISHash -> ERISHash -> Int -> m ()
+erisVerifyKey db level reference key blockSize = do
+  when (level == 0) $ fail "cannot verify a leaf node"
+  node <- erisDereferenceNode db reference key level blockSize
+  let hsh = erisBlake2b node
+  when (hsh /= key) $ fail "key in read capability is invalid"

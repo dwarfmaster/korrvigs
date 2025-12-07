4e869e319ffef904ec24a263e0078b7755a354e5
DwarfMaster <luc@dwarfmaster.net>
2025-12-07 11:04:23 +0100

    Implement in-memory ERIS db

--- lib/Data/ERIS/DB/Class.hs
+++ lib/Data/ERIS/DB/Class.hs
@@ -1,9 +1,25 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Data.ERIS.DB.Class where
 
+import Control.Monad.Reader.Class
+import Control.Monad.State.Class
 import Data.ERIS.Crypto
+import Data.Map (Map)
+import qualified Data.Map as M
 
 class (Monad m) => ERISBlockRead db m where
   erisBlockStorageGet :: db -> ERISHash -> m (Maybe ERISBlock)
 
-class (ERISBlockRead db m) => ERISBlockWrite db m where
+class (Monad m) => ERISBlockWrite db m where
   erisBlockStoragePut :: db -> ERISHash -> ERISBlock -> m ()
+
+data ERISInMemoryDB = ERISInMemoryDB
+
+instance (Monad m, MonadReader (Map ERISHash ERISBlock) m) => ERISBlockRead ERISInMemoryDB m where
+  erisBlockStorageGet ERISInMemoryDB hsh =
+    M.lookup hsh <$> ask
+
+instance (Monad m, MonadState (Map ERISHash ERISBlock) m) => ERISBlockWrite ERISInMemoryDB m where
+  erisBlockStoragePut ERISInMemoryDB hsh block =
+    modify $ M.insert hsh block

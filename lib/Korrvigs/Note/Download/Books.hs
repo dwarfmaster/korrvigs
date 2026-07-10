5840ba1b794b8c709ed7d23469b8b7293864b5d8
DwarfMaster <luc@dwarfmaster.net>
2026-07-10 21:50:47 +0200

    Extract more data from bedetheque imports

--- lib/Korrvigs/Note/Download/Books.hs
+++ lib/Korrvigs/Note/Download/Books.hs
@@ -1,15 +1,21 @@
 module Korrvigs.Note.Download.Books where
 
 import Control.Lens
+import Data.Aeson
+import Data.List.Split (divvy)
+import Data.Maybe
 import Data.Monoid
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Time.Calendar
+import Data.Time.Format
 import Korrvigs.Entry.New
 import Korrvigs.Metadata
 import Korrvigs.Metadata.Media
 import Korrvigs.Metadata.Media.Ontology
 import Korrvigs.Monad
 import Text.HTML.TagSoup
+import Text.Read (readMaybe)
 
 manytoon :: (MonadKorrvigs m) => Text -> [Tag Text] -> m (Endo NewEntry)
 manytoon url _
@@ -31,11 +37,38 @@ goodreads url _
 goodreads _ _ = pure mempty
 
 bedetheque :: (MonadKorrvigs m) => Text -> [Tag Text] -> m (Endo NewEntry)
-bedetheque url _
-  | "https://www.bedetheque.com/" `T.isPrefixOf` url =
-      pure $ Endo $ setMtdtValue MediaMtdt Comic . setMtdtValue Language "fr" . delFeed
+bedetheque url tags
+  | any (`T.isPrefixOf` url) bdgestUrls =
+      pure $ foldMap matchSpans (divvy 2 1 tags) <> foldMap matchMeta tags <> common
   where
+    bdgestUrls = ["https://www.bedetheque.com/", "https://www.bdgest.com/"]
+    common = Endo $ setMtdtValue MediaMtdt Comic . setMtdtValue Language "fr" . delFeed
     delFeed = neMtdt . at (mtdtName Feed) .~ Nothing
+    matchMeta :: Tag Text -> Endo NewEntry
+    matchMeta tag@(TagOpen "meta" attrs)
+      | tag ~== TagOpen ("meta" :: Text) [("itemprop", "datePublished")] =
+          case lookup "content" attrs >>= parseDate of
+            Nothing -> mempty
+            Just day -> Endo $ neDate %~ Just . fromMaybe day
+    matchMeta tag@(TagOpen "meta" attrs)
+      | tag ~== TagOpen ("meta" :: Text) [("itemprop", "description")] =
+          case lookup "content" attrs of
+            Nothing -> mempty
+            Just desc -> Endo $ neMtdt . at (mtdtName Abstract) ?~ toJSON desc
+    matchMeta _ = mempty
+    parseDate :: Text -> Maybe Day
+    parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d" . T.unpack
+    matchSpans :: [Tag Text] -> Endo NewEntry
+    matchSpans [tag@(TagOpen "span" _), TagText isbn]
+      | tag ~== TagOpen ("span" :: Text) [("itemprop", "isbn")] =
+          Endo $ neMtdt . at (mtdtName ISBNMtdt) %~ Just . fromMaybe (toJSON [isbn])
+    matchSpans [tag@(TagOpen "span" _), TagText pages]
+      | tag ~== TagOpen ("span" :: Text) [("itemprop", "numberOfPages")] =
+          case readMaybe (T.unpack pages) of
+            Nothing -> mempty
+            Just (pgs :: Int) ->
+              Endo $ neMtdt . at (mtdtName Pages) %~ Just . fromMaybe (toJSON pgs)
+    matchSpans _ = mempty
 bedetheque _ _ = pure mempty
 
 webtoons :: (MonadKorrvigs m) => Text -> [Tag Text] -> m (Endo NewEntry)
